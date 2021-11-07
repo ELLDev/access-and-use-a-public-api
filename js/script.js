3b40e10650cd43b03c3985176e59a1e0a502ec3b
ratoobi <69602231+ratoobi@users.noreply.github.com>
2021-11-06 23:51:56 -0300

    Add modal window that displays more details about employees

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 let employees = [];
 const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
-email, location, phone, dob &noinfo &nat=US`;
+email, location, phone, dob &noinfo`;
 const employeeList = document.querySelector(".employee-list");
+const overlay = document.querySelector(".overlay");
+const modalContainer = document.querySelector(".modal-content");
+const modalClose = document.querySelector(".modal-close");
 
 fetch(urlAPI)
   .then((res) => res.json())
@@ -19,8 +22,8 @@ function displayEmployees(employeeData) {
     let city = employee.location.city;
     let picture = employee.picture;
 
-    employeeHTML += `
-    <li class="employee data-index="${index}">
+    employeeHTML = `
+    <li class="employee" data-index="${index}">
      <img class="avatar" src="${picture.large}" />
      <div class="employee-details">
        <h3 class="name">${name.first} ${name.last}</h3>
@@ -29,7 +32,51 @@ function displayEmployees(employeeData) {
      </div>
     </li>
     `;
+
+    employeeList.insertAdjacentHTML('beforeend', employeeHTML);
   });
 
-  employeeList.innerHTML = employeeHTML;
 }
+
+displayModal = (index) => {
+  let { dob, email, location, name, phone, picture } = employees[index];
+  let date = new Date(dob.date);
+  const modalHTML = `
+  <img class="avatar" src="${picture.large}" />
+  <div class="text-container">
+  <h2 class="name">${name.first} ${name.last}</h2>
+  <p class="email">${email}</p>
+  <p class="address">${location.city}</p>
+  <hr />
+  <p>${phone}</p>
+  <p class="address">${location.street}, ${location.state} ${
+    location.postcode
+  }</p>
+  <p>Birthday:
+  ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+  </div>
+  `;
+
+  overlay.classList.remove("hidden");
+  modalContainer.innerHTML = modalHTML;
+};
+
+employeeList.addEventListener("click", (e) => {
+  if (e.target.tagName === "LI") {
+    const employeeCard = e.target;
+    const index = employeeCard.getAttribute("data-index");
+    displayModal(index);
+  } else if (e.target.parentElement.tagName === "LI") {
+    const employeeCard = e.target.parentElement;
+    const index = employeeCard.getAttribute("data-index");
+    displayModal(index);
+  } else if (e.target.parentElement.parentElement.tagName === "LI") {
+    const employeeCard = e.target.parentElement.parentElement;
+    const index = employeeCard.getAttribute("data-index");
+    displayModal(index);
+  }
+});
+
+modalClose.addEventListener("click", () => {
+  overlay.classList.add("hidden");
+});
