2cc45395df340c90820605467d8e636d86fe2f2d
ratoobi <69602231+ratoobi@users.noreply.github.com>
2021-11-08 23:51:46 -0300

    Add search bar and navigation keys for the modal window

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
 let employees = [];
+let employeesCopy = [];
 const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
-email, location, phone, dob &noinfo`;
+email, location, phone, dob &noinfo &nat=US`;
 const employeeList = document.querySelector(".employee-list");
 const overlay = document.querySelector(".overlay");
 const modalContainer = document.querySelector(".modal-content");
 const modalClose = document.querySelector(".modal-close");
+let modalIndex = 0;
+let isModalWindowClosed = true;
+const header = document.querySelector("header");
+header.insertAdjacentHTML(
+  "beforeend",
+  `
+<label for="search" class="name-search">
+<span>Search by name</span>
+<input id="search" placeholder="Search by name...">
+<button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+</label>
+`
+);
+const searchBar = document.querySelector("#search");
+const searchButton = document.querySelector("label button");
 
 fetch(urlAPI)
   .then((res) => res.json())
@@ -12,11 +28,18 @@ fetch(urlAPI)
   .then(displayEmployees)
   .catch((err) => console.log(err));
 
-function displayEmployees(employeeData) {
-  employees = employeeData;
-  let employeeHTML = "";
+function displayEmployees(employeeData, firstRun = true) {
+  if (firstRun) {
+    employeesCopy = employeeData;
+    employees = employeeData;
+  } else {
+    employeesCopy = employeeData;
+  }
 
-  employees.forEach((employee, index) => {
+  let employeeHTML = "";
+  employeeList.innerHTML = "";
+
+  employeesCopy.forEach((employee, index) => {
     let name = employee.name;
     let email = employee.email;
     let city = employee.location.city;
@@ -33,9 +56,8 @@ function displayEmployees(employeeData) {
     </li>
     `;
 
-    employeeList.insertAdjacentHTML('beforeend', employeeHTML);
+    employeeList.insertAdjacentHTML("beforeend", employeeHTML);
   });
-
 }
 
 displayModal = (index) => {
@@ -49,9 +71,9 @@ displayModal = (index) => {
   <p class="address">${location.city}</p>
   <hr />
   <p>${phone}</p>
-  <p class="address">${location.street}, ${location.state} ${
-    location.postcode
-  }</p>
+  <p class="address">${location.street.number} ${location.street.name}, ${
+    location.state
+  } ${location.postcode}</p>
   <p>Birthday:
   ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
   </div>
@@ -61,22 +83,73 @@ displayModal = (index) => {
   modalContainer.innerHTML = modalHTML;
 };
 
+searchName = (list, name) => {
+  let regex = new RegExp(name, "i");
+  let newList = [];
+
+  for (let index = 0; index < list.length; index++) {
+    let fullName = list[index].name.first + " " + list[index].name.last;
+    if (
+      regex.test(list[index].name.first) ||
+      regex.test(list[index].name.last) ||
+      regex.test(fullName)
+    ) {
+      newList.push(list[index]);
+    }
+  }
+  displayEmployees(newList, false);
+};
+
 employeeList.addEventListener("click", (e) => {
+  isModalWindowClosed = false;
+
   if (e.target.tagName === "LI") {
     const employeeCard = e.target;
     const index = employeeCard.getAttribute("data-index");
+    modalIndex = parseInt(index);
     displayModal(index);
   } else if (e.target.parentElement.tagName === "LI") {
     const employeeCard = e.target.parentElement;
     const index = employeeCard.getAttribute("data-index");
+    modalIndex = parseInt(index);
     displayModal(index);
   } else if (e.target.parentElement.parentElement.tagName === "LI") {
     const employeeCard = e.target.parentElement.parentElement;
     const index = employeeCard.getAttribute("data-index");
+    modalIndex = parseInt(index);
     displayModal(index);
+  }
+});
+
+// Use left and right arrow keys to navigate through modal windows
+
+document.addEventListener("keyup", (key) => {
+  if (!isModalWindowClosed) {
+    if (key.code === "ArrowLeft") {
+      modalIndex -= 1;
+      if (modalIndex < 0) modalIndex = 0;
+      displayModal(modalIndex);
+    } else if (key.code === "ArrowRight") {
+      modalIndex += 1;
+      displayModal(modalIndex);
+    }
+    if (key.code === "Escape") {
+      overlay.classList.add("hidden");
+      isModalWindowClosed = true;
+      modalIndex = 0;
+    }
   }
 });
 
 modalClose.addEventListener("click", () => {
   overlay.classList.add("hidden");
+  isModalWindowClosed = true;
+  modalIndex = 0;
+});
+
+searchBar.addEventListener("keyup", () => {
+  searchName(employees, searchBar.value);
+});
+searchButton.addEventListener("click", () => {
+  searchName(employees, searchBar.value);
 });
